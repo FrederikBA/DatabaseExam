a8855c0f17b759ed3ced75fb432f812830e33425
FrederikBA <frederikbandersen98@gmail.com>
2023-05-02 11:00:26 +0200

    imports

---
+++ server/database/db_connector.py
@@ -0,0 +1,2 @@
+def get_database():
+    return "Connection established"
--- server/services/test_service.py
+++ server/services/test_service.py
@@ -1,2 +1,12 @@
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+
+from database import db_connector
+
 def get_data():
-    return {"Data": "This is data"}
+    return db_connector.get_database()
