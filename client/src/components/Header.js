2d106a2fe4fafe52b0801a03383bd3ba74773cb1
FrederikBA <frederikbandersen98@gmail.com>
2023-05-18 18:45:40 +0200

    Frontend - navbar padding

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,12 +9,12 @@ const Header = () => {
             <nav>
                 <ul className='header'>
                     <NavLink className="none" to="/"><Logo /></NavLink>
-                    <li><NavLink to='/'>Film</NavLink></li>
+                    <li className="frontpage-nav"><NavLink to='/'>Film</NavLink></li>
                     <li><NavLink to="/searchfilter">Søg Film</NavLink></li>
                     <li><NavLink to="/movielist">Film Liste</NavLink></li>
                     <li><NavLink to="/moviedetails">Film Detaljer</NavLink></li>
                     <li><NavLink to="/cart">Indkøbskurv</NavLink></li>
-                    <li className="align-right"><NavLink to="/login">Log ind</NavLink></li>
+                    <li className="align-right login-nav"><NavLink to="/login">Log ind</NavLink></li>
                 </ul>
             </nav>
         </div >
