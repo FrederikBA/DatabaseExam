61870d43654d500b582ee9f5542c56cb39eaf774
Janussr <Janusstivang@gmail.com>
2023-05-21 14:21:54 +0200

    Title can be read on movie details

--- client/src/components/MovieDetails.js
+++ client/src/components/MovieDetails.js
@@ -1,30 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import apiUtils from '../utils/apiUtils';
 
 const MovieDetails = () => {
     const { movieId } = useParams();
     const [movie, setMovie] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        // Fetch movie data based on movieId
-        // You can replace this with your own logic to fetch the movie data
-        const fetchMovieData = async () => {
-            try {
-                // Make an API call or fetch data from your data source
-                const response = await fetch(`/api/movies/${movieId}`);
-                const data = await response.json();
 
-                setMovie(data); // Update the movie state with the fetched data
-                setIsLoading(false); // Set isLoading to false
-            } catch (error) {
-                console.log(error);
-                setIsLoading(false); // Set isLoading to false in case of an error
-            }
-        };
+    const URL = apiUtils.getUrl()
 
-        fetchMovieData(); // Call the fetchMovieData function
-    }, [movieId]);
+useEffect(() => {
+    const getMovieDetails = async () => {
+        const response = await apiUtils.getAxios().get(URL + `/movies/${movieId}`)
+        setMovie(response.data)
+        setIsLoading(false)
+    }
+    getMovieDetails()
+},[URL, movieId]);
 
     if (isLoading) {
         return <h2>Loading...</h2>;
@@ -37,8 +30,6 @@ const MovieDetails = () => {
     return (
         <div>
             <h2>{movie.title}</h2>
-            <p>Release Year: {movie.release_year}</p>
-            <p>Rating: {movie.rating}</p>
             {/* Render other movie details */}
         </div>
     );
