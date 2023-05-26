1ae4a0a4cf82af7a74d2f6e7ca69ca54abdc991c
FrederikBA <frederikbandersen98@gmail.com>
2023-05-26 11:26:51 +0200

    new queries cus janus does not like sql

--- queries/create_tables.sql
+++ queries/create_tables.sql
@@ -1,19 +1,5 @@
 USE BockBluster;
 
-CREATE TABLE movie (
-  movie_id VARCHAR(255) PRIMARY KEY,
-  price_id INT,
-  title VARCHAR(255),
-  release_year INT,
-  rating FLOAT,
-  poster VARCHAR(255)
-);
-
-CREATE TABLE price (
-  price_id INT PRIMARY KEY,
-  price FLOAT
-);
-
 CREATE TABLE member (
   member_id VARCHAR(255) PRIMARY KEY,
   first_name VARCHAR(255),
@@ -43,20 +29,14 @@ CREATE TABLE loan (
   loan_date DATE,
   return_date DATE,
   FOREIGN KEY (order_id) REFERENCES orders (order_id),
-  FOREIGN KEY (movie_id) REFERENCES movie (movie_id),
 );
 
 ALTER TABLE user_login ADD CONSTRAINT FK_user_login_member
   FOREIGN KEY (member_id) REFERENCES member (member_id);
 
-ALTER TABLE movie ADD CONSTRAINT FK_movie_price
-  FOREIGN KEY (price_id) REFERENCES price (price_id);
 
 ALTER TABLE loan ADD CONSTRAINT FK_loan_orders
   FOREIGN KEY (order_id) REFERENCES orders (order_id);
-
-ALTER TABLE loan ADD CONSTRAINT FK_loan_movie
-  FOREIGN KEY (movie_id) REFERENCES movie (movie_id);
 
 ALTER TABLE orders ADD CONSTRAINT FK_orders_member
   FOREIGN KEY (member_id) REFERENCES member (member_id);
